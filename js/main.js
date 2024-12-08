a7da255353c7f9f381b28b65a5660b2c77451367
Okutepa <okutepa15@gmail.com>
2024-12-08 05:44:16 -0500

    Move project to MAMP htdocs folder for local server testing
    - Moved the project folder to MAMP htdocs directory for testing with a local PHP server.
    - Updated file paths for includes and assets where necessary to ensure compatibility.
    - Verified database connection and dynamic PHP functionality.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+
+gsap.registerPlugin(ScrollTrigger)
 const sidebar = document.querySelector(".sidebar");
 const showSidebarButton = document.querySelector(".hideondesktop a");
 const hideSidebarButton = document.querySelector(".sidebar li:first-child a");
 const player = new Plyr('video');
+const headingCon = document.querySelector('.heading')
+
+gsap.fromTo(headingCon, 
+  {opacity: 0}, {opacity: 1, duration: 1.4});
 
 
 function showSidebar() {
